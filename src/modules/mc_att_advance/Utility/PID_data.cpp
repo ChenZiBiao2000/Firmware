f3ebb7a9a5aedf50323f73f4a1f373e2e07ab1f0
Huang <huangwenfuture@sina.com>
2019-04-26 00:28:21 +0800

    mc_att_advance/Utility: add the folder of Utility which can store the simulink file

---
+++ src/modules/mc_att_advance/Utility/PID_data.cpp
@@ -0,0 +1,36 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+//
+// File: PID_data.cpp
+//
+// Code generated for Simulink model 'PID'.
+//
+// Model version                  : 1.40
+// Simulink Coder version         : 9.0 (R2018b) 24-May-2018
+// C/C++ source code generated on : Sun Apr 21 22:57:45 2019
+//
+// Target selection: ert.tlc
+// Embedded hardware selection: ARM Compatible->ARM Cortex
+// Code generation objectives: Unspecified
+// Validation result: Not run
+//
+#include "PID.h"
+#include "PID_private.h"
+
+// Invariant block signals (default storage)
+const ConstB_PID_T PID_ConstB = {
+	{
+		0.0F,
+		0.0F,
+		0.0F
+	}
+	// '<S7>/Constant'
+};
+
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
---
+++ src/modules/mc_att_advance/Utility/PID_private.h
@@ -0,0 +1,28 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+//
+// File: PID_private.h
+//
+// Code generated for Simulink model 'PID'.
+//
+// Model version                  : 1.40
+// Simulink Coder version         : 9.0 (R2018b) 24-May-2018
+// C/C++ source code generated on : Sun Apr 21 22:57:45 2019
+//
+// Target selection: ert.tlc
+// Embedded hardware selection: ARM Compatible->ARM Cortex
+// Code generation objectives: Unspecified
+// Validation result: Not run
+//
+#ifndef RTW_HEADER_PID_private_h_
+#define RTW_HEADER_PID_private_h_
+#include "rtwtypes.h"
+#endif                                 // RTW_HEADER_PID_private_h_
+
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
