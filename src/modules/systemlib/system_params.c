7983296eff7e4f75bcc0f12de37419680b55fada
Huang <huangwenfuture@sina.com>
2019-05-14 10:57:26 +0800

    modules: add and find the param SYS_MC_ATT

--- src/modules/systemlib/system_params.c
+++ src/modules/systemlib/system_params.c
@@ -278,3 +278,17 @@ PARAM_DEFINE_INT32(SYS_HAS_MAG, 1);
  * @group System
  */
 PARAM_DEFINE_INT32(SYS_HAS_BARO, 1);
+
+/**
+ * Automatically configure attitude controller.
+ *
+ * Set to 0 mc_att_controll controller.  set to 1 to mc_att_advance controller
+ * Platform-specific values are used if available.
+ *
+ * @min 0
+ * @max 1
+ * @value 0 mc_att_controll
+ * @value 1 mc_att_advance
+ * @group System
+ */
+PARAM_DEFINE_INT32(SYS_MC_ATT, 0);
